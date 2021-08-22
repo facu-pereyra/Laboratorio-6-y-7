b823c69eb72f703c7fc80594a64695ff36fad703
facuperey <83043601+facuperey@users.noreply.github.com>
2021-08-22 19:36:47 -0300

    Update version_1.ino

--- grabando_sonido/version_1.ino
+++ grabando_sonido/version_1.ino
@@ -1,22 +1,36 @@
-/*
-Empieza a grabar cuando:
+/*                                    CÓDIGO DE GRABACIÓN -- VERSIÓN 1
+*Empieza a grabar cuando:
   Botonstart==1 
   Llega a la hora Inicial indicada (HoraInicio_i, con i=1,2,3)
-Se detiene la grabación cuando:
+*Se detiene la grabación cuando:
   BotonBreak==1
   Llega a la hora Final indicada (HoraFinal_i, con i=1,2,3)
   
-Hay un salto de fichero si pasa un determinado tiempo, dado por "SaltoFichero". Para modificar el tamaño (en segundos) del
-fichero, se puede cambiar la variable "duracion".
-La variable "siguiente" se utiliza para que cuando se llega al tiempo de SaltoFichero el programa para de
-grabar y automaticamente comienza un nuevo fichero.
-La variable "ControladorFinal" no se puede tocar ya que sino no funciona el salto de fichero.
+*Hay un salto de fichero si pasa un determinado tiempo, dado por "SaltoFichero". Para modificar el tamaño (en minutos) del
+ fichero, se puede cambiar la variable "duracion".
+ 
+*La variable "siguiente" se utiliza para que cuando se llega al tiempo de SaltoFichero el programa para de
+ grabar y automaticamente comienza un nuevo fichero.
 
-El nombre del archivo está compuesto por el día más la hora, así por ejemplo, si se graba un archivo el 24 a las 16:07:35, 
-entonces el archivo se llamará 24160735.wav
+*La variable "ControladorFinal" no se puede tocar ya que sino no funciona el salto de fichero ni para de grabar.
 
-Cuando se toca el botón para grabar y no tiene la tarjeta SD salta error (titila 6 veces el led ROJO). 
-WARNING: EVITAR SACAR LA TARJETA MICRO SD PARA QUE NO SE ROMPA Y PREVENIR MAL FUNCIONAMIENTO DE LA MISMA.
+*Si se empieza a grabar antes de la hora de inicio, la grabación para 2 segundos antes del inicio automático
+ ya que sino se produce error.
+ 
+*Hay 3 intervalos de horas de grabación. Para añadir más intervalos de grabación se tienen que agregar las horas 
+ de inicio y finalizacíon pertinentes, además del "void confHora_i" con "i" siendo el número de intervalo de grabación
+ (por ejemplo, para HoraInicio_5 corresponde void confHora_5).
+ 
+*El nombre del archivo está compuesto por el día más la hora, así por ejemplo, si se graba un archivo el 24 a las 16:07:35, 
+ entonces el archivo se llamará 24160735.wav
+
+*Cuando se toca el botón para grabar y no tiene la tarjeta SD salta error (titila 6 veces el led ROJO) y luego se reinicia 
+ el Arduino. 
+ 
+        ***********
+        **WARNING**: EVITAR SACAR LA TARJETA MICRO SD PARA QUE NO SE ROMPA Y PREVENIR MAL FUNCIONAMIENTO DE LA MISMA.
+        ***********
+
 */
 
 #include <SD.h>
@@ -33,32 +47,31 @@ TMRpcm audio;   // create an object for use in this sketch
 // Variables que se pueden modificar
 
 unsigned long freq_muestreo = 22000; //Frecuencia de muestreo en Hertz
-uint8_t Duracion = 5; //Duracion de los ficheros, esta en minutos
+uint8_t Duracion = 10; //Duracion de los ficheros, esta en minutos
 
-uint8_t HoraInicio_1 = 8;
+uint8_t HoraInicio_1 = 7;
 uint8_t MinInicio_1 = 0;
 uint8_t SecInicio_1 = 0;
 
-uint8_t HoraFinal_1 = 10;
+uint8_t HoraFinal_1 = 9;
 uint8_t MinFinal_1 = 0;
 uint8_t SecFinal_1 = 0;
 
-uint8_t HoraInicio_2 = 14;
+uint8_t HoraInicio_2 = 11;
 uint8_t MinInicio_2 = 0;
 uint8_t SecInicio_2 = 0;
 
-uint8_t HoraFinal_2 = 18;
+uint8_t HoraFinal_2 = 13;
 uint8_t MinFinal_2 = 0;
 uint8_t SecFinal_2 = 0;
 
-//uint8_t HoraInicio_3 = 6;
-//uint8_t MinInicio_3 = 0;
-//uint8_t SecInicio_3 = 0;
+uint8_t HoraInicio_3 = 16;
+uint8_t MinInicio_3 = 0;
+uint8_t SecInicio_3 = 0;
 
-//uint8_t HoraFinal_3 = 7;
-//uint8_t MinFinal_3 = 0;
-//uint8_t SecFinal_3 = 0;
-
+uint8_t HoraFinal_3 = 18;
+uint8_t MinFinal_3 = 0;
+uint8_t SecFinal_3 = 0;
 
 //--------------------------------------------------------------------------------------------
 //Defino variables
@@ -96,6 +109,90 @@ void dateTime(uint16_t* date, uint16_t* time) {
   *time = FAT_TIME(now.hour(), now.minute(), now.second());
 }
 //==============================================================================
+//-------------------------------------------------------------------------------
+void confHora_1(){
+  DateTime now = rtc.now();
+  if (((MinInicio_1-now.minute())==1)||((MinInicio_1-now.minute())==(-59))&&(((HoraInicio_1-now.hour())==1)||((HoraInicio_1-now.hour())==0))){
+    
+     HoraInicio=HoraInicio_1;
+     MinInicio=MinInicio_1;
+     SecInicio=SecInicio_1;
+      
+     HoraFinal=HoraFinal_1;
+     MinFinal=MinFinal_1;
+     SecFinal=SecFinal_1;
+   }
+   if((now.hour()>=(HoraInicio_1))&&((now.hour()<=(HoraFinal_1)))) {
+     
+   HoraInicio=HoraInicio_1;
+     MinInicio=MinInicio_1;
+     SecInicio=SecInicio_1;
+      
+     HoraFinal=HoraFinal_1;
+     MinFinal=MinFinal_1;
+     SecFinal=SecFinal_1;
+   }
+}
+//-------------------------------------------------------------------------------
+void confHora_2(){
+DateTime now = rtc.now();
+if(((MinInicio_2-now.minute())==1)||((MinInicio_2-now.minute())==(-59))&&(((HoraInicio_2-now.hour())==1)||((HoraInicio_2-now.hour())==0))){
+     
+   HoraInicio=HoraInicio_2;
+   MinInicio=MinInicio_2;
+   SecInicio=SecInicio_2;
+      
+   HoraFinal=HoraFinal_2;
+   MinFinal=MinFinal_2;
+   SecFinal=SecFinal_2;
+   }
+  if(((now.hour()>=(HoraInicio_2)))&&((now.hour()<=(HoraFinal_2)))) {
+    
+   HoraInicio=HoraInicio_2;
+   MinInicio=MinInicio_2;
+   SecInicio=SecInicio_2;
+      
+   HoraFinal=HoraFinal_2;
+   MinFinal=MinFinal_2;
+   SecFinal=SecFinal_2;
+   }
+}
+//==============================================================================
+//-------------------------------------------------------------------------------
+void confHora_3(){
+DateTime now = rtc.now();
+if(((MinInicio_3-now.minute())==1)||((MinInicio_3-now.minute())==(-59))&&(((HoraInicio_3-now.hour())==1)||((HoraInicio_3-now.hour())==0))){
+     
+   HoraInicio=HoraInicio_3;
+   MinInicio=MinInicio_3;
+   SecInicio=SecInicio_3;
+      
+   HoraFinal=HoraFinal_3;
+   MinFinal=MinFinal_3;
+   SecFinal=SecFinal_3;
+   }
+  if(((now.hour()>=(HoraInicio_3)))&&((now.hour()<=(HoraFinal_3)))) {
+    
+   HoraInicio=HoraInicio_3;
+   MinInicio=MinInicio_3;
+   SecInicio=SecInicio_3;
+      
+   HoraFinal=HoraFinal_3;
+   MinFinal=MinFinal_3;
+   SecFinal=SecFinal_3;
+   }
+}
+//==============================================================================
+//Luego de haber empezado manualmete una grabacion y se acerque el inicio automatico, 
+//este para de grabar 2 segundos antes para evitar producir un error. 
+void manual_auto(){
+  DateTime now = rtc.now();
+  if (ControladorFinal==1 && ((HoraInicio-now.hour())==1||(HoraInicio-now.hour())==0) && ((MinInicio-now.minute())==1||(MinInicio-now.minute())==(-59)) &&((SecInicio-now.second())==2||(SecInicio-now.second())==(-58))){
+    LedFinal();
+    audio.stopRecording(filename);
+    ControladorFinal=false;     
+  }
+}
 //------------------------------------------------------------------------------
 void getFileName(){
     DateTime now = rtc.now();
@@ -125,7 +222,6 @@ void LedFinal(){
 //==============================================================================
 void setup() {  
     rtc.begin();
-    Serial.begin(9600);
   if (rtc.lostPower()) {
     // following line sets the RTC to the date & time this sketch was compiled (setea el dia y hora de la compu)
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -142,78 +238,34 @@ void setup() {
 }
 void loop() {
   DateTime now = rtc.now();
-  //digitalWrite(LED_Work, HIGH);
-  //digitalWrite(LED_Error, HIGH);
-
-  if (((MinInicio_1-now.minute())==1)||((MinInicio_1-now.minute())==(-59))&&((HoraInicio_1-now.hour())==1)){
-     HoraInicio=HoraInicio_1;
-     MinInicio=MinInicio_1;
-     SecInicio=SecInicio_1;
-      
-     HoraFinal=HoraFinal_1;
-     MinFinal=MinFinal_1;
-     SecFinal=SecFinal_1;
-   }
-   if(((now.hour()>=(HoraInicio_1)))&&((now.hour()<=(HoraFinal_1)))) {
-    HoraInicio=HoraInicio_1;
-     MinInicio=MinInicio_1;
-     SecInicio=SecInicio_1;
-      
-     HoraFinal=HoraFinal_1;
-     MinFinal=MinFinal_1;
-     SecFinal=SecFinal_1;
-   }
-   if(((MinInicio_2-now.minute())==1)||((MinInicio_2-now.minute())==(-59))&&((HoraInicio_2-now.hour())==1)){
-     HoraInicio=HoraInicio_2;
-     MinInicio=MinInicio_2;
-     SecInicio=SecInicio_2;
-      
-     HoraFinal=HoraFinal_2;
-     MinFinal=MinFinal_2;
-     SecFinal=SecFinal_2;
-   }
-  if(((now.hour()>=(HoraInicio_2)))&&((now.hour()<=(HoraFinal_2)))) {
-    HoraInicio=HoraInicio_2;
-     MinInicio=MinInicio_2;
-     SecInicio=SecInicio_2;
-      
-     HoraFinal=HoraFinal_2;
-     MinFinal=MinFinal_2;
-     SecFinal=SecFinal_2;
-   }
-//   else if(((MinInicio_3-now.minute())==1)||((MinInicio_3-now.minute())==(-59))&&((HoraInicio_3-now.hour())==1)){
-//     HoraInicio=HoraInicio_3;
-//     MinInicio=MinInicio_3;
-//     SecInicio=SecInicio_3;
-//     
-//     HoraFinal=HoraFinal_3;
-//     MinFinal=MinFinal_3;
-//     SecFinal=SecFinal_3;
-//   }
-    Serial.print(now.minute()); Serial.print(":"); Serial.println(now.second());      
+  
+  confHora_1();
+  confHora_2();
+  confHora_3();
+  
   if (!digitalRead(BotonStart)||(Siguiente==1)||(now.hour()==HoraInicio && now.minute()==MinInicio && now.second()==SecInicio)) { 
     if (!SD.begin(SD_ChipSelectPin)) LedError();
-    Serial.print("Antes de getFile: ");Serial.print(now.minute()); Serial.print(":"); Serial.println(now.second());    
     getFileName();    
     SdFile::dateTimeCallback(dateTime); //Para poner los atributos en el archivo guardado
     audio.startRecording(filename,freq_muestreo,MIC);
-    Serial.print("Despues de startRecord: ");Serial.print(now.minute()); Serial.print(":"); Serial.println(now.second());    
     t=millis();
     Siguiente=false;
     ControladorFinal=true;
     digitalWrite(LED_Work, HIGH);
   }
+  
+  manual_auto();
+  
   if ((millis() - t > SaltoFichero)&&(ControladorFinal==1)){        
-      digitalWrite(LED_Work, LOW);
-      Siguiente=true;
-      audio.stopRecording(filename);
+    digitalWrite(LED_Work, LOW);
+    Siguiente=true;
+    audio.stopRecording(filename);
   }   
-  else if (!digitalRead(BotonBreak)||(now.hour()==HoraFinal && now.minute()==MinFinal && now.second()==SecFinal)){           
-       audio.stopRecording(filename);
-       Serial.print("Despues de stopRecord: ");Serial.print(now.minute()); Serial.print(":"); Serial.println(now.second());    
-       LedFinal();
-       Siguiente=false;
-       ControladorFinal=false;
-       return; 
+  else if ((!digitalRead(BotonBreak)||(now.hour()==HoraFinal && now.minute()==MinFinal && now.second()==SecFinal))&&(ControladorFinal==1)){           
+    audio.stopRecording(filename);
+    LedFinal();
+    Siguiente=false;
+    ControladorFinal=false;
+    return; 
   }
 } //cierra void loop
