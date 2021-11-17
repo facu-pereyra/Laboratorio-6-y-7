64681bb78b40553bdee17b9f008963fabf5f25f7
facuperey <83043601+facuperey@users.noreply.github.com>
2021-11-16 23:51:46 -0300

    Update BC_speaker.ino

--- Grabador_Reproductor/BC_speaker.ino
+++ Grabador_Reproductor/BC_speaker.ino
@@ -3,7 +3,10 @@
  * 
  * Los archivos wav a reproducir se encuentran en la carpeta Audio
  * 
- * Los audios se reproducen de forma aleatoria mediante el nombre "ave_i" donde i=0, 1, 2, 3,...
+ * Los audios se reproducen de forma aleatoria mediante el nombre "pajaro_i" donde i=0, 1, 2, 3,...
+ * 
+ * Con el botón "BotonBreak" cortas la reproducción y no se vuelve a reproducir a no ser que se reinicie el
+ * arduino.
  * 
  */
 #include <SD.h>                   
@@ -22,8 +25,13 @@ uint8_t num_cantos = 3; // Indica cuantos cantos hay en la carpeta de reproducci
 
 char fileName[20];    
 uint8_t fileToPlay;
+uint8_t Transistor = 3;
+uint8_t Pin_Speaker = 4; //Pin de entrada que da el inicio o finalización del speaker
 uint8_t LED_Error = 5;
-uint8_t Speaker_pin = 6; //Pin de entrada que da el inicio o finalización del speaker
+uint8_t RST_PIN = 6;
+uint8_t BotonBreak = 7; 
+
+volatile boolean Controlador=true;
 //==============================================================================
 void LedError() {
   digitalWrite(LED_Error, LOW);  
@@ -33,30 +41,42 @@ void LedError() {
     digitalWrite(LED_Error, LOW);
     delay(1000);      
   }
-  //pinMode(RST_PIN,  OUTPUT);
-  //digitalWrite(RST_PIN, LOW);
+  pinMode(RST_PIN,  OUTPUT);
+  digitalWrite(RST_PIN, LOW);
 }
 //==============================================================================
 void setup(){
-  Serial.begin(9600);
-  pinMode(Speaker_pin,INPUT);
-  pinMode(LED_Error,OUTPUT);  
-  tmrpcm.speakerPin = 9;   
+  pinMode(Pin_Speaker,INPUT_PULLUP);
+  pinMode(LED_Error,OUTPUT);
+  pinMode(BotonBreak, INPUT_PULLUP);
+  pinMode(Transistor, OUTPUT);
+  
+  tmrpcm.speakerPin = 9;
+  digitalWrite(Transistor,HIGH);
+  delay(500);   
   if (!SD.begin(SD_ChipSelectPin)) LedError(); 
-  randomSeed(analogRead(A7)); 
+  randomSeed(analogRead(A7));
+  digitalWrite(Transistor,LOW); 
 }
 
 void loop(){   
-    while(digitalRead(Speaker_pin)==1){
-      if(tmrpcm.isPlaying()==0){ 
+    while(digitalRead(Pin_Speaker)==0){      
+      digitalWrite(Transistor,HIGH);
+      delay(500);
+      if(Controlador==1 && tmrpcm.isPlaying()==0){
+        delay(20000); 
         fileToPlay = random(0,num_cantos);
-        sprintf(fileName, "Audio/ave_%d.wav", fileToPlay);  
+        sprintf(fileName, "Audio/pajaro_%d.wav", fileToPlay);  
         tmrpcm.play(fileName);
-        Serial.println(fileName);
+      }
+      if(!digitalRead(BotonBreak)){
+        tmrpcm.stopPlayback();
+        Controlador=false;
       }
     }
-    if(digitalRead(Speaker_pin)==0){
+    if(digitalRead(Pin_Speaker)==1){
       tmrpcm.stopPlayback();
-      delay(2000); //Dejar esta línea mientras se prueba pq pasa rapido de 0 a 1
+      digitalWrite(Transistor,LOW);
     }
 }
+
